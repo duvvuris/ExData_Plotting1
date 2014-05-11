c124a00ac15a40ade56157be0b019c5c17362029
duvvuris <duvvurisrinivas@hotmail.com>
2014-05-11 13:26:15 +0530

    minor corrections

--- plot3.R
+++ plot3.R
@@ -20,13 +20,10 @@ mysubsetData$DateTime <- strptime(paste(mysubsetData$Date, mysubsetData$Time), f
 
 png(file="plot3.png", width=480, height=480)
 
-plot(mysubsetData$DateTime, mysubsetData$Sub_metering_1, type="l" )
+plot(mysubsetData$DateTime, mysubsetData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(mysubsetData$DateTime, mysubsetData$Sub_metering_2, col ="Red")
 lines(mysubsetData$DateTime, mysubsetData$Sub_metering_3, col ="Blue")
 legend("topright", lty= 1, col = c("black","red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.off()
-
-
-
 
