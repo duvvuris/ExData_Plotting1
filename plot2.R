ba701e1b05baac791bb688931f20cf51e57260b0
duvvuris <duvvurisrinivas@hotmail.com>
2014-05-11 13:07:17 +0530

    Source file for second plot

---
+++ plot2.R
@@ -0,0 +1,29 @@
+# This project is the first of the four source files to plot as per the project instructions
+# This program reads the requried data and writes it to .csv file for further work
+
+# set the working directory 
+
+setwd("c:/work/expdata/exdata-data-household_power_consumption")
+
+
+# read the subset data for the two days , This file was created by reading the subset data and 
+# writing back as a csv file in the first assingment 
+
+mysubsetData <- read.csv("subsetdata_household_power_consumption.csv")
+
+
+#create a new data column of data and time 
+
+mysubsetData$DateTime <- strptime(paste(mysubsetData$Date, mysubsetData$Time), format="%d/%m/%Y %H:%M:%S")
+
+
+#Initialize the .png file for writing the plot to file 
+
+png(file="plot1.png", width=480, height=480)
+plot(mysubsetData$DateTime, mysubsetData$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+dev.off()
+
+
+
+
